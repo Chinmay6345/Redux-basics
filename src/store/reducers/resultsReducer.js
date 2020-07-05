e77fe9aff457bb0c88dd78aa45c2ad150f7e291d
Chinmay <Chinmay8116@gmail.com>
2020-07-05 17:25:01 +0530

    added delete function

--- src/store/reducers/resultsReducer.js
+++ src/store/reducers/resultsReducer.js
@@ -3,14 +3,17 @@ import { updateObject } from '../HelperMethods/Utility';
 const initialState = {
     results :new Array()
 }
+function deleteResult(state,action) {
+    const updatedArr= state.results.filter(result=>result.id !==action.id);
+    return updateObject(state,{results: updatedArr});
+}
 
 const reducer = (state = initialState, action) => {
     switch(action.type) {      
         case actiontypes.STORE_RESULT:
             return updateObject(state,{results: state.results.concat({id: Math.random() * (352364643 - 45) + 3, value: action.result})});
         case actiontypes.DELETERESULT:
-            const updatedArr= state.results.filter(result=>result.id !==action.id);
-            return updateObject(state,{results: updatedArr});
+            return deleteResult(state,action);
     }
     return state;
 };
