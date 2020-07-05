84965c7ea557bb3007729b15a64c24ed6be75207
Chinmay <Chinmay8116@gmail.com>
2020-07-05 17:12:15 +0530

    added utility

--- src/store/reducers/counterReducer.js
+++ src/store/reducers/counterReducer.js
@@ -1,11 +1,29 @@
 import * as actiontypes from '../constants/constants';
-
+import { updateObject } from '../HelperMethods/Utility';
 const initialState = {
     counter: 0
 }
 
 const reducer = (state = initialState, action) => {
     switch(action.type) {
+        case actiontypes.INCREMENT:
+           return updateObject(state,{counter:state.counter+1})
+        case actiontypes.DECREMENT:
+            return updateObject(state,{counter:state.counter-1})
+        case actiontypes.ADD5:
+            return updateObject(state,{counter:state.counter+action.value})
+        case actiontypes.SUBTRACT5:
+            return updateObject(state,{counter:state.counter-action.value})
+    }
+    return state;
+};
+
+export default reducer;
+
+
+/**
+ * 
+ *  switch(action.type) {
         case actiontypes.INCREMENT:
             return {
                 ...state,
@@ -27,7 +45,4 @@ const reducer = (state = initialState, action) => {
                 counter: state.counter-action.value
             };
     }
-    return state;
-};
-
-export default reducer;
+ */
